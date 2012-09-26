86bd6328f51c8fde2479fc14c000b5f250a93259
David S Anderson <ande3577@gmail.com>
2012-09-25 21:05:21 -0500

    Fixed typo in migrate

--- db/migrate/013_add_time_tracker_enabled.rb
+++ db/migrate/013_add_time_tracker_enabled.rb
@@ -3,6 +3,6 @@ class AddTimeTrackerEnabled < ActiveRecord::Migration
     add_column :user_preferences, :time_tracker_enabled, :boolean, :default => true
   end
   def down
-    remove_columne :user_preferences, :time_tracker_enabled
+    remove_column :user_preferences, :time_tracker_enabled
   end
 end
