4027d2e4268f5445e5dc1239a98f396fe227121d
David S Anderson <ande3577@gmail.com>
2012-09-28 17:15:18 -0500

    Closes #12: no longer closes wrong time tracker when issue closed

--- lib/tt_issue_patch.rb
+++ lib/tt_issue_patch.rb
@@ -18,7 +18,7 @@ module IssuePatch
       logger.debug "Issue changes: #{self.changes}"
       if self.closed?() and status_id_changed? and  Setting.plugin_redmine_time_tracker[:stop_on_close]
         logger.debug "Time tracker post save hook called: issue #{self.id}: #{self.subject}"
-        time_trackers = TimeTracker.where(:issue_id == self.id)
+        time_trackers = TimeTracker.where("issue_id = ?", self.id)
         time_trackers.each do |time_tracker|
           logger.debug "Stopping time tracker for #{time_tracker.user.name()}, issue = #{time_tracker.issue_id}"
           time_tracker.stop
