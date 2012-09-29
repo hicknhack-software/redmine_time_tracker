7cbf1eee229185b7f87d6270cc91b0bbf0ba0c34
David S Anderson <ande3577@gmail.com>
2012-09-29 11:59:27 -0500

    Closes #24: now shows comment after stop if no project/issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,22 +46,24 @@ module ApplicationHelper
   
   def time_tracker_link(user, options)
      time_tracker = time_tracker_for(user) 
-     if !time_tracker.nil? 
-        # A time tracker exists, display the stop action
-       if time_tracker.issue_id.nil? && !time_tracker.project_id.nil?
-         link_to l(:stop_time_tracker).capitalize + ': ' + Project.find(time_tracker.project_id).name,
-                    {:controller => '/time_trackers', :action => 'stop', :time_tracker => {}},
-                    :class => 'icon icon-stop'
-       else
-         link_to l(:stop_time_tracker).capitalize + ' #' + time_tracker.issue_id.to_s,
-           {:controller => '/time_trackers', :action => 'stop', :time_tracker => {}},
-           :class => 'icon icon-stop'
+     if !time_tracker.nil?
+       stop_label = '' 
+       # A time tracker exists, display the stop action
+       if !time_tracker.issue_id.nil?
+         stop_label += ' #' + time_tracker.issue_id.to_s
+       elsif !time_tracker.project_id.nil?
+         stop_label += ' ' + Project.find(time_tracker.project_id).name unless Project.find(time_tracker.project_id).nil?
+       else stop_label += ' ' + time_tracker.comments.to_s
        end 
+       
+       link_to l(:stop_time_tracker).capitalize + stop_label,
+                   {:controller => '/time_trackers', :action => 'stop', :time_tracker => {}},
+                   :class => 'icon icon-stop'
      elsif !options[:project].nil? and user.allowed_to?(:use_time_tracker_plugin, nil, :global => true) and user.allowed_to?(:log_time, options[:project])
         # No time tracker is running, but the user has the rights to track time on this issue 
         # Display the start time tracker action
        if options[:issue].nil?
-         link_to l(:start_time_tracker).capitalize + ': ' +  options[:project].name,
+         link_to l(:start_time_tracker).capitalize + ' ' +  options[:project].name,
              {:controller => '/time_trackers', :action => 'start', :time_tracker => {:project_id => options[:project].id}},
            :class => 'icon icon-start'
        else
